dab5c83d1a970c21d87cf10e937bca7257b4e06b
Mateus Bazani <mrbazani@gmail.com>
2022-01-02 15:03:00 -0300

    feat: altera estrutura do projeto para o curso 3. Adiciona metrica para verificar tempo de renderizacao da classe view.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -27,6 +27,7 @@ export abstract class View<T> {
   protected abstract template(model: T): string;
 
   public update(model: T): void {
+    const t1 = performance.now();
     let template = this.template(model);
 
     if (this.escapar) {
@@ -34,5 +35,8 @@ export abstract class View<T> {
     }
 
     this.elemento.innerHTML = template;
+    const t2 = performance.now();
+
+    console.log(`Tempo de execução do método update: ${(t2 - t1) / 1000} segundos`);
   }
 }
